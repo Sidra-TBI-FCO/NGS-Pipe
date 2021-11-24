bbaef02738b77bd6bb5cf289b8dd2dd1a0be00a9
Eiman-Ahmed <eimanahmed30@gmail.com>
2021-11-24 13:33:02 +0300

    Create strelka2_bcbionextgen_1.1.5_testing.sh

---
+++ Scripts/WES/06.0 - Variants Calling - INDEL/strelka2_bcbionextgen_1.1.5_testing.sh
@@ -0,0 +1,6 @@
+# indel variant calling
+module unload bcbio-nextgen/1.1.1
+module load bcbio-nextgen/1.1.5_testing
+cd /gpfs/projects/tmedicine/TBILAB/JSREP1/WGS/strelka2/Tumor_Normal_JSREP_1_50/work
+COMMAND="bcbio_nextgen.py ../config/Tumor_Normal_JSREP_1_50.yaml -n 100 --timeout 20000 -s lsf -q normal -r P=batch1_50_WGS"
+echo $COMMAND | bsub -n 4 -P variant_calling_JSREP -e vc_error.txt -o vc_output.txt
