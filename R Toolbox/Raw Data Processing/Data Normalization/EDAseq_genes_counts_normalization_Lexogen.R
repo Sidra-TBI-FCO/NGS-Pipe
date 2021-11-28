fcffa507d4f72622709f5d90f08eadcc6881873a
Eiman-Ahmed <eimanahmed30@gmail.com>
2021-11-28 10:53:16 +0300

    Create EDAseq_genes_counts_normalization_Lexogen.R

---
+++ R Toolbox/Raw Data Processing/Data Normalization/EDAseq_genes_counts_normalization_Lexogen.R
@@ -0,0 +1,79 @@
+# Setup environment
+rm(list=ls())
+load("~/R.Config.Rdata")
+setwd(master.location)
+setwd(paste0(master.location,"/TBI-LAB - Nanomaterials - Laura -SDR400025/"))
+
+# dependencies
+source (paste0(toolbox.path,"./R scripts/ipak.function.R"))
+ibiopak(c("EDASeq", "base64enc", "preprocessCore", "openxlsx"))
+
+#Read text file
+# Load required files
+load(paste0(toolbox.path,"./GeneInfo/geneInfo.Sept2018.RData"))
+counts = "./Raw Counts/DB_SDR400025_201202_LEX8_B1.combined.counts.txt"
+sub.reads = readLines(counts, n=1)  #why using (readLines)
+raw.counts = read.csv(counts, sep = "\t",  stringsAsFactors = F)
+
+samples.data = raw.counts
+
+samples.data = aggregate(samples.data,FUN="mean", by=list(samples.data$symbol))
+
+#Edit samples.data format
+#Rownames 
+rownames(samples.data) = samples.data$Group.1
+
+samples.data$id = NULL
+samples.data$symbol = NULL
+samples.data$Group.1 = NULL
+
+#convert samples.data into matrix 
+samples.matrix = as.matrix(samples.data)
+
+#Save data as Rdata file
+#dir.create("./Graphene/processed_data/normalization", showWarnings = FALSE)
+save(samples.data, file = "./Graphene/processed_data/normalization/Unnormalized_counts_EDAseq.Rdata")
+
+# Set geneInfo as data.freme 
+geneInfo = as.data.frame(geneInfo)
+
+#Extract genes from geneInfo that matches genes in samples.matrix
+# unique = returns a vector, data frame or array like x but with duplicate elements/rows removed.
+common.genes = unique(rownames(samples.matrix)[which(rownames(samples.matrix) %in% rownames(geneInfo))])
+
+# Extract the common genes for geneInfo and samples.matrix
+geneInfo = geneInfo[common.genes,]
+samples.filtered = samples.matrix[common.genes,]
+
+mode(samples.filtered) = "numeric"
+dim(samples.filtered)  #rows = 18869, columns = 14
+
+#samples.filtered; the final file before normalization 
+save(samples.filtered, file = "./Graphene/processed_data/normalization/unnormalized_counts_geneInfo_filtered_EDAseq.Rdata")
+
+# Data Normalization using EDASeq
+samples.exp.norm = newSeqExpressionSet(samples.filtered, featureData = geneInfo)
+
+
+# removes effect related to between lane distributional differences, as sequencing depth
+samples.exp.norm = betweenLaneNormalization(samples.exp.norm, which = "upper", offset = T)
+
+#Take= log (unnormalized + .1) + offst(normalized)
+samples.norm.log = log(samples.filtered +.1) + offst(samples.exp.norm)
+samples.norm.log = floor(exp(samples.norm.log) - .1)  #return non decimal values
+
+#Quantile Normalization
+samples.quantiles.norm = normalize.quantiles(samples.norm.log)
+samples.quantiles.norm = floor(samples.quantiles.norm)
+
+rownames(samples.quantiles.norm) = rownames(samples.norm.log)
+colnames(samples.quantiles.norm) = colnames(samples.norm.log)
+
+#Log transformation (final normalized-transformed file)
+quant.Log2.transformed = log(samples.quantiles.norm+1,2) #log base 2
+dim(quant.Log2.transformed)
+
+save(quant.Log2.transformed, samples.quantiles.norm, geneInfo, file="./Analysis/Normalization/normalized_counts.Rdata")
+
+
+
